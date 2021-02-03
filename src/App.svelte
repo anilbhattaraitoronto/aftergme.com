20b410828639330510aa7361c907470398435c0d
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2021-02-03 11:37:25 -0500

    First commit -- project set up

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Router from "svelte-spa-router";
+    import Home from "./comps/pages/Home.svelte";
+
+    const routes = {
+        "/": Home,
+    };
+</script>
+
+<main>
+    <Router {routes} />
+</main>
+
+<style>
+    main {
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+</style>
