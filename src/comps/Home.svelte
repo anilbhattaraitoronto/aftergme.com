dc74de0adf721c1d3c8dc6507e042bb182720b87
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2021-02-03 19:26:28 -0500

    Added figcaption to landing image

--- src/comps/Home.svelte
+++ src/comps/Home.svelte
@@ -7,6 +7,14 @@
         src="https://images.unsplash.com/photo-1468254095679-bbcba94a7066?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHdhbGxzdHJlZXQlMjBiZXRzfGVufDB8fDB8&auto=format&fit=crop&w=400&q=60"
         alt="Wall Street street sign in New York"
     />
+    <figure>
+        <figcaption>
+            Source: <a
+                href="https://images.unsplash.com/photo-1468254095679-bbcba94a7066?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHdhbGxzdHJlZXQlMjBiZXRzfGVufDB8fDB8&auto=format&fit=crop&w=400&q=60"
+                >Unsplash</a
+            >
+        </figcaption>
+    </figure>
 </div>
 <p>
     Everone who is directly involved in stock trading no longer can ignore
